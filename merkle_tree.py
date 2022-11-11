dcb43c117989268c6f8bb5447e597fba61aad963
somnef <a.m.bousselat@gmail.com>
2022-11-11 22:34:20 +0100

    cleanup

--- merkle_tree.py
+++ merkle_tree.py
@@ -10,32 +10,12 @@ class MerkleNode:
         self.child_2 = child_2
 
 
-class MerkleTree:
-    def __init__(self, root: MerkleNode) -> None:
-        self.root_node = root
-
-
-def md5_list(l):
-    return [hashlib.md5(elem.encode()).hexdigest() for elem in l]
-
-
-def pair_concat(l):
-    new_l = []
-    for i in range(0, len(l), 2):
-        if i != len(l) - 1:
-            new_l.append(l[i] + l[i+1])
-        else:
-            new_l.append(l[i] + l[i])
-
-    return new_l
-
-
 if len(sys.argv) < 2:
     print(f"usage: {sys.argv[0]} <elem_1> <elem_2> ... <elem_n>")
     exit(0)
 
 blocks = sys.argv[1:]
-hashed_blocks = md5_list(blocks)
+hashed_blocks = [hashlib.md5(elem.encode()).hexdigest() for elem in blocks]
 
 merkle_nodes = []
 for i in range(len(hashed_blocks)):
