fe72b4e885a4115236813c0951a669ee242f7d09
somnef <a.m.bousselat@gmail.com>
2022-11-11 22:31:18 +0100

    improved algorithm and can now access the whole tree

--- merkle_tree.py
+++ merkle_tree.py
@@ -1,5 +1,19 @@
 import hashlib
 import sys
+from typing import Union
+
+
+class MerkleNode:
+    def __init__(self, val: str, child_1: Union['MerkleNode', None], child_2: Union['MerkleNode', None]) -> None:
+        self.value = val
+        self.child_1 = child_1
+        self.child_2 = child_2
+
+
+class MerkleTree:
+    def __init__(self, root: MerkleNode) -> None:
+        self.root_node = root
+
 
 def md5_list(l):
     return [hashlib.md5(elem.encode()).hexdigest() for elem in l]
@@ -23,9 +37,25 @@ if len(sys.argv) < 2:
 blocks = sys.argv[1:]
 hashed_blocks = md5_list(blocks)
 
-while len(blocks) > 1:
-    blocks = pair_concat(hashed_blocks)
-    hashed_blocks = md5_list(blocks)
+merkle_nodes = []
+for i in range(len(hashed_blocks)):
+    leaf = MerkleNode(hashed_blocks[i], None, None)
+    merkle_nodes.append(leaf)
 
-root = hashed_blocks[0]
-print(root)
+new_merkle_nodes = merkle_nodes.copy()
+while len(merkle_nodes) > 1:
+    new_merkle_nodes = []
+
+    for i in range(0, len(merkle_nodes), 2):
+        idx = i+1 if (i != len(merkle_nodes) - 1) else i
+
+        s = (merkle_nodes[i].value + merkle_nodes[idx].value).encode()
+        val = hashlib.md5(s).hexdigest()
+        node = MerkleNode(val, merkle_nodes[i], merkle_nodes[idx])
+        new_merkle_nodes.append(node)
+
+    merkle_nodes = new_merkle_nodes.copy()
+
+
+merkle_root = merkle_nodes[0]
+print(merkle_root.value)
